e86e2cb3f13dac027693172065bb6cb03129bf19
Billy Hao <billyhao12@gmail.com>
2024-07-08 11:44:34 -0700

    begin building consonant scoring matrix

---
+++ app/lib/consonantScoringMatrix.ts
@@ -0,0 +1,27 @@
+const consonantScoringMatrix = {
+    B: {},
+    CH: {},
+    D: {},
+    DH: {},
+    F: {},
+    G: {},
+    JH: {},
+    K: {},
+    L: {},
+    M: {},
+    N: {},
+    NG: {},
+    P: {},
+    R: {},
+    S: {},
+    SH: {},
+    T: {},
+    TH: {},
+    V: {},
+    Z: {},
+    ZH: {},
+    "_*": {},
+    "*_": {}
+}
+
+export default consonantScoringMatrix;
