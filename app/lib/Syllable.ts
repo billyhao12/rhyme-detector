44a37ab2ea7d708ddd159dcf3377ce97256e2a75
Billy Hao <billyhao12@gmail.com>
2024-07-12 07:57:39 -0700

    code formatting

--- app/lib/Syllable.ts
+++ app/lib/Syllable.ts
@@ -1,96 +1,108 @@
 class Syllable {
-    #stress: number = -1;
-    #wordLength: number = 1;
-    #wordPosition: number = 0;
+  #stress: number = -1;
+  #wordLength: number = 1;
+  #wordPosition: number = 0;
 
-    #startingConsonants: Array<string> = [];
-    #vowel: string = '';
-    #endingConsonants: Array<string> = [];
+  #startingConsonants: Array<string> = [];
+  #vowel: string = "";
+  #endingConsonants: Array<string> = [];
 
-    constructor(startingConsonants: Array<string>, vowel: string, endingConsonants: Array<string>) {
-        this.#startingConsonants = startingConsonants;
-        this.#parseVowel(vowel);
-        this.#endingConsonants = endingConsonants;
+  constructor(
+    startingConsonants: Array<string>,
+    vowel: string,
+    endingConsonants: Array<string>
+  ) {
+    this.#startingConsonants = startingConsonants;
+    this.#parseVowel(vowel);
+    this.#endingConsonants = endingConsonants;
+  }
+
+  #parseVowel(vowel: string) {
+    if (vowel.length > 2) {
+      this.#stress = parseInt(vowel.substring(2));
+      this.#vowel = vowel.substring(0, 2);
+    } else {
+      this.#stress = -1;
+      this.#vowel = vowel;
+    }
+  }
+
+  addEndingConsonant(endingConsonant: string) {
+    this.#endingConsonants.push(endingConsonant);
+  }
+
+  removeStartingConsonants() {
+    this.#startingConsonants.length = 0;
+  }
+
+  set startingConsonants(value: Array<string>) {
+    this.#startingConsonants = value;
+  }
+
+  equals(other: Syllable): boolean {
+    if (this.#vowel !== other.#vowel) return false;
+    if (this.#stress !== other.#stress) return false;
+    if (this.#startingConsonants.length !== other.#startingConsonants.length)
+      return false;
+    if (this.#endingConsonants.length !== other.#endingConsonants.length)
+      return false;
+    for (let i = 0; i < this.#startingConsonants.length; i++) {
+      if (this.#startingConsonants[i] !== other.#startingConsonants[i])
+        return false;
+    }
+    for (let i = 0; i < this.#endingConsonants.length; i++) {
+      if (this.#endingConsonants[i] !== other.#endingConsonants[i])
+        return false;
     }
 
-    #parseVowel(vowel: string) {
-        if (vowel.length > 2) {
-            this.#stress = parseInt(vowel.substring(2));
-            this.#vowel = vowel.substring(0, 2);
-        } else {
-            this.#stress = -1;
-            this.#vowel = vowel;
-        }
-    }
+    return true;
+  }
 
-    addEndingConsonant(endingConsonant: string) {
-        this.#endingConsonants.push(endingConsonant);
-    }
+  get stress(): number {
+    return this.#stress;
+  }
 
-    removeStartingConsonants() {
-        this.#startingConsonants.length = 0;
+  get vowel(): string {
+    if (this.#stress < 0) {
+      return this.#vowel;
+    } else {
+      return this.#vowel + this.#stress;
     }
+  }
 
-    set startingConsonants(value: Array<string>) {
-        this.#startingConsonants = value;
-    }
+  get startingConsonants(): Array<string> {
+    return this.#startingConsonants;
+  }
 
-    equals(other: Syllable): boolean {
-        if (this.#vowel !== other.#vowel) return false;
-        if (this.#stress !== other.#stress) return false;
-        if (this.#startingConsonants.length !== other.#startingConsonants.length) return false;
-        if (this.#endingConsonants.length !== other.#endingConsonants.length) return false;
-        for (let i = 0; i < this.#startingConsonants.length; i++) {
-            if (this.#startingConsonants[i] !== other.#startingConsonants[i]) return false;
-        }
-        for (let i = 0; i < this.#endingConsonants.length; i++) {
-            if (this.#endingConsonants[i] !== other.#endingConsonants[i]) return false;
-        }
+  get endingConsonants(): Array<string> {
+    return this.#endingConsonants;
+  }
 
-        return true;
-    }
+  hasStress(): boolean {
+    return this.#stress >= 0;
+  }
 
-    get stress(): number {
-        return this.#stress;
-    }
+  setWord(length: number, position: number) {
+    this.#wordLength = length;
+    this.#wordPosition = position;
+  }
 
-    get vowel(): string {
-        if (this.#stress < 0) {
-            return this.#vowel;
-        } else {
-            return this.#vowel + this.#stress;
-        }
-    }
+  get wordLength(): number {
+    return this.#wordLength;
+  }
 
-    get startingConsonants(): Array<string> {
-        return this.#startingConsonants;
-    }
+  get wordPosition(): number {
+    return this.#wordPosition;
+  }
 
-    get endingConsonants(): Array<string> {
-        return this.#endingConsonants;
-    }
-
-    hasStress(): boolean {
-        return this.#stress >= 0;
-    }
-
-    setWord(length: number, position: number) {
-        this.#wordLength = length;
-        this.#wordPosition = position;
-    }
-
-    get wordLength(): number {
-        return this.#wordLength;
-    }
-
-    get wordPosition(): number {
-        return this.#wordPosition;
-    }
-
-    toString(): string {
-        return this.startingConsonants.toString() + " "
-                + this.vowel
-                + (this.hasStress() ? this.stress : "") + " "
-                + this.endingConsonants.toString();
-    }
+  toString(): string {
+    return (
+      this.startingConsonants.toString() +
+      " " +
+      this.vowel +
+      (this.hasStress() ? this.stress : "") +
+      " " +
+      this.endingConsonants.toString()
+    );
+  }
 }
