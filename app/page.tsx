9dff7a6729f67002ecb055bd996a0f44c3db49c8
Billy Hao <billyhao12@gmail.com>
2024-10-20 17:43:43 -0700

    updating styling using pure Tailwind CSS classes

--- app/page.tsx
+++ app/page.tsx
@@ -4,42 +4,50 @@ import clsx from "clsx";
 import { FormEvent, MutableRefObject, useEffect, useRef } from "react";
 
 export default function Home() {
-  const lyricsInputRef: MutableRefObject<null | HTMLDivElement> = useRef(null);
+  // const lyricsInputRef: MutableRefObject<null | HTMLDivElement> = useRef(null);
 
-  useEffect(() => {
-    if (lyricsInputRef.current) {
-      lyricsInputRef.current.focus();
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (lyricsInputRef.current) {
+  //     lyricsInputRef.current.focus();
+  //   }
+  // }, []);
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
+  // const handleSubmit = (event: FormEvent) => {
+  //   event.preventDefault();
 
-    const userInputLyrics = lyricsInputRef.current?.innerText;
-    console.log({ userInputLyrics });
+  //   const userInputLyrics = lyricsInputRef.current?.innerText;
+  //   console.log({ userInputLyrics });
 
-    if (userInputLyrics) {
-      const lines = userInputLyrics.split("\n");
-      for (let i = 0; i < lines.length; i++) {
-        console.log(`line ${i}: ${lines[i]}`);
-      }
-    }
-  };
+  //   if (userInputLyrics) {
+  //     const lines = userInputLyrics.split("\n");
+  //     for (let i = 0; i < lines.length; i++) {
+  //       console.log(`line ${i}: ${lines[i]}`);
+  //     }
+  //   }
+  // };
 
   return (
-    <main className={styles.main}>
-      <h1 className={styles.heading}>Rhyme Detector</h1>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <div>Enter rap lyrics to highlight below</div>
-        <div
-          id="lyricsInput"
-          ref={lyricsInputRef}
-          className={styles.lyricsInput}
-          contentEditable
-        ></div>
-        <button type="submit" className={clsx(styles.btn, styles.btnBlue)}>
-          Highlight Rhymes
-        </button>
+    <main className="w-full flex flex-col p-8">
+      <h1 className="w-full flex justify-center p-4 text-3xl">
+        Rhyme Detector
+      </h1>
+      <form className="container mx-auto p-4">
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-4">
+          <div className="p-4">
+            <div>Enter lyrics to highlight below</div>
+            <textarea className="w-full h-screen p-4" />
+          </div>
+          <div className="p-4 flex justify-center items-center">
+            <button type="submit" className={clsx(styles.btn, styles.btnBlue)}>
+              Highlight Rhymes
+            </button>
+          </div>
+          <div className="p-4">
+            <div className="w-full bg-white h-screen text-black p-4">
+              <span>test</span>
+            </div>
+          </div>
+        </div>
       </form>
     </main>
   );
