244be7faf9fece5720f17035e4c0cfb819f26347
Billy Hao <billyhao12@gmail.com>
2024-10-21 23:52:28 -0700

    Static Page (#1)
    * updated styling using pure Tailwind CSS classes
    * use @apply in sass file
    * form element contains textarea element and submit button
    * track lyrics input with react state
    * install axios

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,46 @@
 "use client";
+import { useState } from "react";
 import styles from "./page.module.sass";
 import clsx from "clsx";
-import { FormEvent, MutableRefObject, useEffect, useRef } from "react";
 
 export default function Home() {
-  const lyricsInputRef: MutableRefObject<null | HTMLDivElement> = useRef(null);
-
-  useEffect(() => {
-    if (lyricsInputRef.current) {
-      lyricsInputRef.current.focus();
-    }
-  }, []);
-
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-
-    const userInputLyrics = lyricsInputRef.current?.innerText;
-    console.log({ userInputLyrics });
-
-    if (userInputLyrics) {
-      const lines = userInputLyrics.split("\n");
-      for (let i = 0; i < lines.length; i++) {
-        console.log(`line ${i}: ${lines[i]}`);
-      }
-    }
-  };
+  const [lyricsInput, setLyricsInput] = useState("");
 
   return (
     <main className={styles.main}>
-      <h1 className={styles.heading}>Rhyme Detector</h1>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <div>Enter rap lyrics to highlight below</div>
-        <div
-          id="lyricsInput"
-          ref={lyricsInputRef}
-          className={styles.lyricsInput}
-          contentEditable
-        ></div>
-        <button type="submit" className={clsx(styles.btn, styles.btnBlue)}>
-          Highlight Rhymes
-        </button>
-      </form>
+      <h1 className={styles.title}>Rhyme Detector</h1>
+      <div className={styles.mainContainer}>
+        <div className={styles.mainGrid}>
+          <form className={styles.form}>
+            <div className={styles.lyricsInputContainer}>
+              <label htmlFor="lyricsInput" className="block py-4">
+                Enter lyrics to highlight below
+              </label>
+              <textarea
+                id="lyricsInput"
+                name="lyricsInput"
+                value={lyricsInput}
+                onChange={(e) => setLyricsInput(e.target.value)}
+                className={styles.lyricsInput}
+              />
+            </div>
+            <div className={styles.middleContainer}>
+              <button
+                type="submit"
+                className={clsx(styles.btn, styles.btnBlue)}
+              >
+                Highlight Rhymes
+              </button>
+            </div>
+          </form>
+          <div className={styles.lyricsOutputContainer}>
+            <div className="py-4">Highlighted rhymes</div>
+            <div className={styles.lyricsOutput}>
+              <span>test</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
