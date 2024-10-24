3f15391ae6640060e5d3caf5f709d76d73f3e7ac
Billy Hao <billyhao12@gmail.com>
2024-10-24 15:56:36 -0700

    Working Implementation of Multisyllable Rhyme Highlighting (#3)
    * updating styling using pure Tailwind CSS classes
    
    * adjust column sizes
    
    * use more @apply
    
    * more styling migration to use @apply
    
    * update breakpoints for columns
    
    * wrap form only around input elements and button
    
    * use html label with textarea
    
    * track lyrics input with react state
    
    * install axios
    
    * change tab width to 4
    
    * update `.gitignore`
    
    * remove legacy code
    
    * create utils to handle api endpoints
    
    * successfully calling multisyllable endpoint
    
    * output every word as a <span>
    
    * update style page
    
    * multisyllable highlights working - need to add spaces between words
    
    * add a space between words in the output

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,75 @@
 "use client";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import styles from "./page.module.sass";
 import clsx from "clsx";
+import multisyllableApi from "./lib/api/Multisyllable";
 
 export default function Home() {
     const [lyricsInput, setLyricsInput] = useState("");
+    const [lyricsOutput, setLyricsOutput] = useState<Array<JSX.Element>>([]);
+
+    const createClsxForWord = (word: string, style: Array<string>) => {
+        let clsxResult = [];
+
+        let containsTwoTextDecorations = false;
+        if (style.includes("underline") && style.includes("strikethrough")) {
+            containsTwoTextDecorations = true;
+            clsxResult.push(styles.underlineAndStrikethrough);
+        }
+
+        for (let i = 0; i < style.length; i++) {
+            if (containsTwoTextDecorations) {
+                if (style[i] === "underline" || style[i] === "strikethrough") {
+                    continue;
+                }
+            }
+
+            clsxResult.push(styles[style[i]]);
+        }
+
+        return clsxResult;
+    };
+
+    const createSpanEl = (word: string, style: Array<string>) => {
+        const clsxResult = createClsxForWord(word, style);
+        return <span className={clsx(...clsxResult)}>{word}</span>;
+    };
+
+    const handleSubmit = async (e: React.SyntheticEvent) => {
+        e.preventDefault();
+        const axiosData = await multisyllableApi.highlightMultisyllableRhymes({
+            lyrics: lyricsInput
+        });
+
+        const outputLyricsData = axiosData?.data?.lyrics;
+        const lyricsOutputInProgress = [];
+        if (outputLyricsData) {
+            for (let i = 0; i < outputLyricsData.length; i++) {
+                const line = outputLyricsData[i];
+                for (let j = 0; j < line.length; j++) {
+                    const spanEl = (
+                        <Fragment>
+                            {createSpanEl(line[j].word, line[j].style)}
+                            {/* add a space after the span element if it's not the last word in the line */}
+                            {j < line.length - 1 ? " " : ""}
+                        </Fragment>
+                    );
+
+                    lyricsOutputInProgress.push(spanEl);
+                }
+                lyricsOutputInProgress.push(<br />);
+            }
+
+            setLyricsOutput(lyricsOutputInProgress);
+        }
+    };
 
     return (
         <main className={styles.main}>
             <h1 className={styles.title}>Rhyme Detector</h1>
             <div className={styles.mainContainer}>
                 <div className={styles.mainGrid}>
-                    <form className={styles.form}>
+                    <form className={styles.form} onSubmit={handleSubmit}>
                         <div className={styles.lyricsInputContainer}>
                             <label htmlFor="lyricsInput" className="block py-4">
                                 Enter lyrics to highlight below
@@ -36,7 +94,7 @@ export default function Home() {
                     <div className={styles.lyricsOutputContainer}>
                         <div className="py-4">Highlighted rhymes</div>
                         <div className={styles.lyricsOutput}>
-                            <span>test</span>
+                            {lyricsOutput.map((spanEl) => spanEl)}
                         </div>
                     </div>
                 </div>
