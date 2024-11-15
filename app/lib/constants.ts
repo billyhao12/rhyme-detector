7d3a1e6f012661d9324aaee1e2d6e71798a6eb90
Billy Hao <billyhao12@gmail.com>
2024-11-14 23:38:30 -0800

    New Monosyllable Rhyme Type (#5)
    * add monosyllable endpoint to api lib
    
    * add highlight class to sass file
    
    * dropdown component with rhyme styles
    
    * create a general `handleSubmit` function
    
    * frontend processing for monosyllable rhymes in `highlightMonosyllableRhymes` function
    
    * disable highlight button if no lyrics in input box or no rhyme style selected

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -2,8 +2,11 @@
  * Constants file to store strings as variables.
  */
 
+export const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
 export const HTTP = {
     ENDPOINT: {
+        MONOSYLLABLE: "rhymes/monosyllable",
         MULTISYLLABLE: "rhymes/multisyllable"
     },
     METHOD: {
@@ -11,4 +14,8 @@ export const HTTP = {
     }
 };
 
-export const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+export enum RHYME_STYLE_OPTIONS {
+    SELECT_A_RHYME_STYLE = "Select a Rhyme Style",
+    MONOSYLLABLE = "Monosyllable",
+    MULTISYLLABLE = "Multisyllable"
+};
--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,17 @@
 import { Fragment, useState } from "react";
 import styles from "./page.module.sass";
 import clsx from "clsx";
+import monosyllableApi from "./lib/api/Monosyllable";
 import multisyllableApi from "./lib/api/Multisyllable";
+import RhymeStyleDropdown from "./lib/components/dropdown";
+import { RHYME_STYLE_OPTIONS } from "./lib/constants";
 
 export default function Home() {
     const [lyricsInput, setLyricsInput] = useState("");
     const [lyricsOutput, setLyricsOutput] = useState<Array<JSX.Element>>([]);
+    const [rhymeStyle, setRhymeStyle] = useState(
+        RHYME_STYLE_OPTIONS.SELECT_A_RHYME_STYLE
+    );
 
     /**
      * @param style - an array of styles (e.g. ["bold", "italic"])
@@ -50,17 +56,50 @@ export default function Home() {
     };
 
     /**
+     * Calls the monosyllable API endpoint and creates a span element
+     * with the appropriate styles for each word.
+     *
+     * Sets the lyricsOutput state to an array of Fragment elements with a <br />
+     * element separating the lines. Fragment elements contain span elements
+     * separated by spaces.
+     */
+    const highlightMonosyllableRhymes = async () => {
+        const axiosData = await monosyllableApi.highlightMonosyllableRhymes({
+            lyrics: lyricsInput
+        });
+
+        const outputLyricsData = axiosData?.data?.lyrics;
+        const lyricsOutputInProgress = [];
+        if (outputLyricsData) {
+            for (let i = 0; i < outputLyricsData.length; i++) {
+                const line = outputLyricsData[i];
+                for (let j = 0; j < line.length; j++) {
+                    const spanEl = (
+                        <Fragment>
+                            {createSpanEl(line[j].word, line[j].style)}
+                            {/* add a space after the span element if it's not the last word in the line */}
+                            {j < line.length - 1 ? " " : ""}
+                        </Fragment>
+                    );
+
+                    lyricsOutputInProgress.push(spanEl);
+                }
+                lyricsOutputInProgress.push(<br />);
+            }
+
+            setLyricsOutput(lyricsOutputInProgress);
+        }
+    };
+
+    /**
      * Calls the multisyllable API endpoint and creates a span element
      * with the appropriate styles for each word.
      *
      * Sets the lyricsOutput state to an array of Fragment elements with a <br />
      * element separating the lines. Fragment elements contain span elements
      * separated by spaces.
-     *
-     * @param e - Event object
      */
-    const handleSubmit = async (e: React.SyntheticEvent) => {
-        e.preventDefault();
+    const highlightMultisyllableRhymes = async () => {
         const axiosData = await multisyllableApi.highlightMultisyllableRhymes({
             lyrics: lyricsInput
         });
@@ -88,6 +127,21 @@ export default function Home() {
         }
     };
 
+    /**
+     * Highlights rhymes depending on the rhyme style selected.
+     *
+     * @param e - Event object
+     */
+    const handleSubmit = async (e: React.SyntheticEvent) => {
+        e.preventDefault();
+
+        if (rhymeStyle === RHYME_STYLE_OPTIONS.MONOSYLLABLE) {
+            await highlightMonosyllableRhymes();
+        } else if (rhymeStyle === RHYME_STYLE_OPTIONS.MULTISYLLABLE) {
+            await highlightMultisyllableRhymes();
+        }
+    };
+
     return (
         <main className={styles.main}>
             <h1 className={styles.title}>Rhyme Detector</h1>
@@ -107,9 +161,18 @@ export default function Home() {
                             />
                         </div>
                         <div className={styles.middleContainer}>
+                            <RhymeStyleDropdown
+                                rhymeStyle={rhymeStyle}
+                                setRhymeStyle={setRhymeStyle}
+                            />
                             <button
                                 type="submit"
                                 className={clsx(styles.btn, styles.btnBlue)}
+                                disabled={
+                                    !lyricsInput ||
+                                    rhymeStyle ===
+                                        RHYME_STYLE_OPTIONS.SELECT_A_RHYME_STYLE
+                                }
                             >
                                 Highlight Rhymes
                             </button>
