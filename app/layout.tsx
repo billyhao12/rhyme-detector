2c9680bae963f822bcfe949cbfb67447102124a1
Billy Hao <billyhao12@gmail.com>
2024-11-18 22:44:42 -0800

    Revert "use flowbite-react to build radio list group"
    This reverts commit 873171771b0774edbf7e3b327059dd54aa6dd420.

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,10 @@ export default function RootLayout({
                     content="width=device-width, initial-scale=1.0"
                 />
             </head>
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                {children}
+                <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
+            </body>
         </html>
     );
 }
--- app/lib/components/radioListGroup.tsx
+++ app/lib/components/radioListGroup.tsx
@@ -1,43 +1,87 @@
 /**
  * A radio list group component
- * Based on code from https://flowbite-react.com/docs/components/forms#radio-button
+ * Based on code from https://flowbite.com/docs/forms/radio/#radio-list-group
  */
 
-import React from "react";
-import { Label, Radio } from "flowbite-react";
+import React, { Fragment } from "react";
 
 const RadioListGroup = () => {
     return (
-        <fieldset className="flex max-w-md flex-col gap-4">
-            <legend className="mb-4">Choose your favorite country</legend>
-            <div className="flex items-center gap-2">
-                <Radio
-                    id="united-state"
-                    name="countries"
-                    value="USA"
-                    defaultChecked
-                />
-                <Label htmlFor="united-state">United States</Label>
-            </div>
-            <div className="flex items-center gap-2">
-                <Radio id="germany" name="countries" value="Germany" />
-                <Label htmlFor="germany">Germany</Label>
-            </div>
-            <div className="flex items-center gap-2">
-                <Radio id="spain" name="countries" value="Spain" />
-                <Label htmlFor="spain">Spain</Label>
-            </div>
-            <div className="flex items-center gap-2">
-                <Radio id="uk" name="countries" value="United Kingdom" />
-                <Label htmlFor="uk">United Kingdom</Label>
-            </div>
-            <div className="flex items-center gap-2">
-                <Radio id="china" name="countries" value="China" disabled />
-                <Label htmlFor="china" disabled>
-                    China (disabled)
-                </Label>
-            </div>
-        </fieldset>
+        <Fragment>
+            <h3 className="mb-4 font-semibold text-gray-900 dark:text-white">
+                Identification
+            </h3>
+            <ul className="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+                    <div className="flex items-center ps-3">
+                        <input
+                            id="list-radio-license"
+                            type="radio"
+                            value=""
+                            name="list-radio"
+                            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                        />
+                        <label
+                            htmlFor="list-radio-license"
+                            className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                        >
+                            Driver License{" "}
+                        </label>
+                    </div>
+                </li>
+                <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+                    <div className="flex items-center ps-3">
+                        <input
+                            id="list-radio-id"
+                            type="radio"
+                            value=""
+                            name="list-radio"
+                            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                        />
+                        <label
+                            htmlFor="list-radio-id"
+                            className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                        >
+                            State ID
+                        </label>
+                    </div>
+                </li>
+                <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+                    <div className="flex items-center ps-3">
+                        <input
+                            id="list-radio-military"
+                            type="radio"
+                            value=""
+                            name="list-radio"
+                            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                        />
+                        <label
+                            htmlFor="list-radio-military"
+                            className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                        >
+                            US Military
+                        </label>
+                    </div>
+                </li>
+                <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+                    <div className="flex items-center ps-3">
+                        <input
+                            id="list-radio-passport"
+                            type="radio"
+                            value=""
+                            name="list-radio"
+                            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                        />
+                        <label
+                            htmlFor="list-radio-passport"
+                            className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                        >
+                            US Passport
+                        </label>
+                    </div>
+                </li>
+            </ul>
+        </Fragment>
     );
 };
 
