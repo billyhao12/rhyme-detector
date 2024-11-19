7eb0a7bfe7c167dd02471f9c9cb41b33026f3ba4
Billy Hao <billyhao12@gmail.com>
2024-11-18 23:49:38 -0800

    make radio group colors darker

--- app/lib/components/rhymeTypesRadioGroup.tsx
+++ app/lib/components/rhymeTypesRadioGroup.tsx
@@ -18,15 +18,15 @@ const RhymeTypesRadioGroup = ({
     return (
         <Fragment>
             <h3 className="text-white">Select a rhyme type</h3>
-            <ul className="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg overflow-hidden">
-                <li className="w-full border-b border-gray-200 rounded-t-lg">
+            <ul className="w-48 text-sm font-medium text-gray-900 bg-slate-50 rounded-lg overflow-hidden">
+                <li className="w-full border-b border-gray-300 rounded-t-lg">
                     <div className="flex items-center ps-3">
                         <input
                             id={RHYME_TYPE_OPTIONS.MONOSYLLABLE}
                             type="radio"
                             value={RHYME_TYPE_OPTIONS.MONOSYLLABLE}
                             name="rhymeTypes"
-                            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2"
+                            className="w-4 h-4 text-blue-600 bg-gray-200 border-gray-400 focus:ring-blue-500 focus:ring-2"
                             checked={
                                 rhymeType === RHYME_TYPE_OPTIONS.MONOSYLLABLE
                             }
@@ -44,14 +44,14 @@ const RhymeTypesRadioGroup = ({
                         </label>
                     </div>
                 </li>
-                <li className="w-full border-b border-gray-200 rounded-t-lg">
+                <li className="w-full border-b border-gray-300 rounded-t-lg">
                     <div className="flex items-center ps-3">
                         <input
                             id={RHYME_TYPE_OPTIONS.MULTISYLLABLE}
                             type="radio"
                             value={RHYME_TYPE_OPTIONS.MULTISYLLABLE}
                             name="rhymeTypes"
-                            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2"
+                            className="w-4 h-4 text-blue-600 bg-gray-200 border-gray-400 focus:ring-blue-500 focus:ring-2"
                             checked={
                                 rhymeType === RHYME_TYPE_OPTIONS.MULTISYLLABLE
                             }
