4bd9c78f3026c529ca843a967722a08b887fdaa7
Billy Hao <billyhao12@gmail.com>
2024-11-18 21:45:14 -0800

    example radio list group from flowbite

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import styles from "./page.module.sass";
 import clsx from "clsx";
 import monosyllableApi from "./lib/api/Monosyllable";
 import multisyllableApi from "./lib/api/Multisyllable";
-import RhymeStyleDropdown from "./lib/components/dropdown";
+import RadioListGroup from "./lib/components/radioListGroup";
 import { RHYME_STYLE_OPTIONS } from "./lib/constants";
 
 export default function Home() {
@@ -161,10 +161,7 @@ export default function Home() {
                             />
                         </div>
                         <div className={styles.middleContainer}>
-                            <RhymeStyleDropdown
-                                rhymeStyle={rhymeStyle}
-                                setRhymeStyle={setRhymeStyle}
-                            />
+                            <RadioListGroup />
                             <button
                                 type="submit"
                                 className={clsx(styles.btn, styles.btnBlue)}
