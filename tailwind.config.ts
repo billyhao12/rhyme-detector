873171771b0774edbf7e3b327059dd54aa6dd420
Billy Hao <billyhao12@gmail.com>
2024-11-18 22:30:29 -0800

    use flowbite-react to build radio list group

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,12 @@
 import type { Config } from "tailwindcss";
+import flowbite from "flowbite-react/tailwind";
 
 const config: Config = {
     content: [
         "./pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./components/**/*.{js,ts,jsx,tsx,mdx}",
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
-        "./node_modules/flowbite/**/*.js"
+        flowbite.content()
     ],
     theme: {
         extend: {
@@ -16,7 +17,7 @@ const config: Config = {
             }
         }
     },
-    plugins: [require("flowbite/plugin")]
+    plugins: [flowbite.plugin()]
 };
 
 export default config;
