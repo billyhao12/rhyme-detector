b8d47717a88e1807f59f652c958ca578b7a5bdb9
Billy Hao <billyhao12@gmail.com>
2024-12-11 01:29:33 -0800

    escape quotes in html

--- app/lib/components/HowToUse.tsx
+++ app/lib/components/HowToUse.tsx
@@ -22,8 +22,8 @@ const HowToUse = () => {
                 the syllables for each pair of lines and highlights words that
                 contain at least one perfect monosyllable rhyme. This means that
                 the vowel and ending consonants sounds for at least one syllable
-                must match. An example of this is the word "mind" rhyming with
-                "blind".
+                must match. An example of this is the word &quot;mind&quot;
+                rhyming with &quot;blind&quot;.
             </p>
 
             <br />
@@ -41,10 +41,11 @@ const HowToUse = () => {
                 which highlights imperfect and multisyllable rhymes in addition
                 to perfect monosyllable ones. This algorithm also examines two
                 lines at a time. Rhymes can span multiple words. For example,
-                "palms are sweaty" rhymes with "arms are heavy". In addition,
-                multiple text decorations indicate that a word or phrase
-                contains more than one rhyme pair. The text decorations used
-                are: bold, italics, red text, underline, and strikethrough.
+                &quot;palms are sweaty&quot; rhymes with &quot;arms are
+                heavy&quot;. In addition, multiple text decorations indicate
+                that a word or phrase contains more than one rhyme pair. The
+                text decorations used are: bold, italics, red text, underline,
+                and strikethrough.
             </p>
         </div>
     );
