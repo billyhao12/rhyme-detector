af6342f4ea109cb6bdccb2dff0b8fae58e46c8ca
Billy Hao <billyhao12@gmail.com>
2025-01-13 08:12:40 -0800

    add a loading state for the submit button

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 /**
  * Contains form elements for user to interact with and highlight rhymes.
  * Makes calls to the backend server.
+ * 
+ * Source for loading spinner: https://flowbite.com/docs/components/spinner/
  */
 
 "use client";
@@ -17,6 +19,7 @@ export default function Home() {
     const [lyricsInput, setLyricsInput] = useState("");
     const [lyricsOutput, setLyricsOutput] = useState<Array<JSX.Element>>([]);
     const [rhymeType, setRhymeType] = useState(RHYME_TYPE_OPTIONS.MONOSYLLABLE);
+    const [isLoading, setIsLoading] = useState(false);
 
     /**
      * @param style - an array of styles (e.g. ["bold", "italic"])
@@ -63,9 +66,16 @@ export default function Home() {
      * separated by spaces.
      */
     const highlightMonosyllableRhymes = async () => {
-        const axiosData = await monosyllableApi.highlightMonosyllableRhymes({
-            lyrics: lyricsInput
-        });
+        let axiosData;
+        setIsLoading(true);
+
+        try {
+            axiosData = await monosyllableApi.highlightMonosyllableRhymes({
+                lyrics: lyricsInput
+            });
+        } finally {
+            setIsLoading(false);
+        }
 
         const outputLyricsData = axiosData?.data?.lyrics;
         const lyricsOutputInProgress = [];
@@ -99,9 +109,16 @@ export default function Home() {
      * separated by spaces.
      */
     const highlightMultisyllableRhymes = async () => {
-        const axiosData = await multisyllableApi.highlightMultisyllableRhymes({
-            lyrics: lyricsInput
-        });
+        let axiosData;
+        setIsLoading(true);
+
+        try {
+            axiosData = await multisyllableApi.highlightMultisyllableRhymes({
+                lyrics: lyricsInput
+            });
+        } finally {
+            setIsLoading(false);
+        }
 
         const outputLyricsData = axiosData?.data?.lyrics;
         const lyricsOutputInProgress = [];
@@ -171,9 +188,32 @@ export default function Home() {
                             <button
                                 type="submit"
                                 className={clsx(styles.btn, styles.btnBlue)}
-                                disabled={!lyricsInput}
+                                disabled={!lyricsInput || isLoading}
                             >
-                                Highlight Rhymes
+                                {isLoading ? (
+                                    <Fragment>
+                                        <svg
+                                            aria-hidden="true"
+                                            role="status"
+                                            className="inline w-4 h-4 me-3 text-white animate-spin"
+                                            viewBox="0 0 100 101"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                                fill="#E5E7EB"
+                                            />
+                                            <path
+                                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                                fill="currentColor"
+                                            />
+                                        </svg>
+                                        Loading...
+                                    </Fragment>
+                                ) : (
+                                    "Highlight Rhymes"
+                                )}
                             </button>
                         </div>
                     </form>
