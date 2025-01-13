bada63779daedb6e2cd333b6916f8aef6a581886
Billy Hao <billyhao12@gmail.com>
2025-01-13 09:03:30 -0800

    Loading State and Error Toast (#10)

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 /**
  * Contains form elements for user to interact with and highlight rhymes.
  * Makes calls to the backend server.
+ * 
+ * Source for loading spinner: https://flowbite.com/docs/components/spinner/
+ * Source for error toast: https://flowbite.com/docs/components/toast/#colors
  */
 
 "use client";
@@ -12,11 +15,16 @@ import multisyllableApi from "./lib/api/Multisyllable";
 import RhymeTypesRadioGroup from "./lib/components/RhymeTypesRadioGroup";
 import { RHYME_TYPE_OPTIONS } from "./lib/constants";
 import HowToUse from "./lib/components/HowToUse";
+import { isAxiosError } from "axios";
 
 export default function Home() {
     const [lyricsInput, setLyricsInput] = useState("");
     const [lyricsOutput, setLyricsOutput] = useState<Array<JSX.Element>>([]);
     const [rhymeType, setRhymeType] = useState(RHYME_TYPE_OPTIONS.MONOSYLLABLE);
+    const [isLoading, setIsLoading] = useState(false);
+    const [displayErrorToast, setDisplayErrorToast] = useState(false);
+    const [errorName, setErrorName] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
 
     /**
      * @param style - an array of styles (e.g. ["bold", "italic"])
@@ -63,9 +71,23 @@ export default function Home() {
      * separated by spaces.
      */
     const highlightMonosyllableRhymes = async () => {
-        const axiosData = await monosyllableApi.highlightMonosyllableRhymes({
-            lyrics: lyricsInput
-        });
+        let axiosData;
+        setIsLoading(true);
+
+        try {
+            axiosData = await monosyllableApi.highlightMonosyllableRhymes({
+                lyrics: lyricsInput
+            });
+        } catch (err) {
+            console.error(err);
+            if (isAxiosError(err)) {
+                setErrorName(err.name);
+                setErrorMessage(err.message);
+                setDisplayErrorToast(true);
+            }
+        } finally {
+            setIsLoading(false);
+        }
 
         const outputLyricsData = axiosData?.data?.lyrics;
         const lyricsOutputInProgress = [];
@@ -99,9 +121,23 @@ export default function Home() {
      * separated by spaces.
      */
     const highlightMultisyllableRhymes = async () => {
-        const axiosData = await multisyllableApi.highlightMultisyllableRhymes({
-            lyrics: lyricsInput
-        });
+        let axiosData;
+        setIsLoading(true);
+
+        try {
+            axiosData = await multisyllableApi.highlightMultisyllableRhymes({
+                lyrics: lyricsInput
+            });
+        } catch (err) {
+            console.error(err);
+            if (isAxiosError(err)) {
+                setErrorName(err.name);
+                setErrorMessage(err.message);
+                setDisplayErrorToast(true);
+            }
+        } finally {
+            setIsLoading(false);
+        }
 
         const outputLyricsData = axiosData?.data?.lyrics;
         const lyricsOutputInProgress = [];
@@ -143,6 +179,52 @@ export default function Home() {
 
     return (
         <main className={styles.main}>
+            {displayErrorToast && (
+                <div
+                    id="toast-danger"
+                    className="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800 fixed"
+                    role="alert"
+                >
+                    <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
+                        <svg
+                            className="w-5 h-5"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                        >
+                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z" />
+                        </svg>
+                        <span className="sr-only">Error icon</span>
+                    </div>
+                    <div className="ms-3 text-sm font-normal">
+                        {`${errorName}: ${errorMessage}`}
+                    </div>
+                    <button
+                        type="button"
+                        className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+                        onClick={() => setDisplayErrorToast(false)}
+                        aria-label="Close"
+                    >
+                        <span className="sr-only">Close</span>
+                        <svg
+                            className="w-3 h-3"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 14 14"
+                        >
+                            <path
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                            />
+                        </svg>
+                    </button>
+                </div>
+            )}
             <h1 className={styles.title}>Rhyme Detector</h1>
             <div className={styles.mainContainer}>
                 <HowToUse />
@@ -171,9 +253,32 @@ export default function Home() {
                             <button
                                 type="submit"
                                 className={clsx(styles.btn, styles.btnBlue)}
-                                disabled={!lyricsInput}
+                                disabled={!lyricsInput || isLoading}
                             >
-                                Highlight Rhymes
+                                {isLoading ? (
+                                    <Fragment>
+                                        <svg
+                                            aria-hidden="true"
+                                            role="status"
+                                            className="inline w-4 h-4 me-3 text-white animate-spin"
+                                            viewBox="0 0 100 101"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                                fill="#E5E7EB"
+                                            />
+                                            <path
+                                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                                fill="currentColor"
+                                            />
+                                        </svg>
+                                        Loading...
+                                    </Fragment>
+                                ) : (
+                                    "Highlight Rhymes"
+                                )}
                             </button>
                         </div>
                     </form>
