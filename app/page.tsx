cc082f04ff8a89fc82ab7d038475d6c6ff5f576a
Billy Hao <billyhao12@gmail.com>
2025-03-05 22:12:01 -0800

    new "Show Rhyme Pairs" button

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,10 @@ export default function Home() {
     const [lyricsInput, setLyricsInput] = useState("");
     const [lyricsOutput, setLyricsOutput] = useState<Array<JSX.Element>>([]);
     const [rhymeType, setRhymeType] = useState(RHYME_TYPE_OPTIONS.MONOSYLLABLE);
-    const [isLoading, setIsLoading] = useState(false);
+    const [isHighlightRhymesLoading, setIsHighlightRhymesLoading] =
+        useState(false);
+    const [isShowRhymePairsLoading, setIsShowRhymePairsLoading] =
+        useState(false);
     const [displayErrorToast, setDisplayErrorToast] = useState(false);
     const [errorName, setErrorName] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
@@ -85,7 +88,7 @@ export default function Home() {
      */
     const highlightMonosyllableRhymes = async () => {
         let axiosData;
-        setIsLoading(true);
+        setIsHighlightRhymesLoading(true);
 
         try {
             axiosData = await monosyllableApi.highlightMonosyllableRhymes({
@@ -99,7 +102,7 @@ export default function Home() {
                 setDisplayErrorToast(true);
             }
         } finally {
-            setIsLoading(false);
+            setIsHighlightRhymesLoading(false);
         }
 
         const outputLyricsData = axiosData?.data?.lyrics;
@@ -140,7 +143,7 @@ export default function Home() {
      */
     const highlightMultisyllableRhymes = async () => {
         let axiosData;
-        setIsLoading(true);
+        setIsHighlightRhymesLoading(true);
 
         try {
             axiosData = await multisyllableApi.highlightMultisyllableRhymes({
@@ -154,7 +157,7 @@ export default function Home() {
                 setDisplayErrorToast(true);
             }
         } finally {
-            setIsLoading(false);
+            setIsHighlightRhymesLoading(false);
         }
 
         const outputLyricsData = axiosData?.data?.lyrics;
@@ -190,7 +193,7 @@ export default function Home() {
      *
      * @param e - Event object
      */
-    const handleSubmit = async (e: React.SyntheticEvent) => {
+    const handleHighlightRhymesOnClick = async (e: React.SyntheticEvent) => {
         e.preventDefault();
 
         if (rhymeType === RHYME_TYPE_OPTIONS.MONOSYLLABLE) {
@@ -252,7 +255,7 @@ export default function Home() {
             <div className={styles.mainContainer}>
                 <HowToUse />
                 <div className={styles.mainGrid}>
-                    <form className={styles.form} onSubmit={handleSubmit}>
+                    <form className={styles.form}>
                         <div className={styles.lyricsInputContainer}>
                             <label
                                 htmlFor="lyricsInput"
@@ -280,36 +283,75 @@ export default function Home() {
                                 rhymeType={rhymeType}
                                 setRhymeType={setRhymeType}
                             />
-                            <button
-                                type="submit"
-                                className={clsx(styles.btn, styles.btnBlue)}
-                                disabled={!lyricsInput || isLoading}
-                            >
-                                {isLoading ? (
-                                    <Fragment>
-                                        <svg
-                                            aria-hidden="true"
-                                            role="status"
-                                            className="inline w-4 h-4 me-3 text-white animate-spin"
-                                            viewBox="0 0 100 101"
-                                            fill="none"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                        >
-                                            <path
-                                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                                                fill="#E5E7EB"
-                                            />
-                                            <path
-                                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                                                fill="currentColor"
-                                            />
-                                        </svg>
-                                        Loading...
-                                    </Fragment>
-                                ) : (
-                                    "Highlight Rhymes"
-                                )}
-                            </button>
+                            <div className={styles.btnGroup}>
+                                <button
+                                    onClick={handleHighlightRhymesOnClick}
+                                    className={clsx(styles.btn, styles.btnBlue)}
+                                    disabled={
+                                        !lyricsInput || isHighlightRhymesLoading
+                                    }
+                                >
+                                    {isHighlightRhymesLoading ? (
+                                        <Fragment>
+                                            <svg
+                                                aria-hidden="true"
+                                                role="status"
+                                                className="inline w-4 h-4 me-3 text-white animate-spin"
+                                                viewBox="0 0 100 101"
+                                                fill="none"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                            >
+                                                <path
+                                                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                                    fill="#E5E7EB"
+                                                />
+                                                <path
+                                                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                                    fill="currentColor"
+                                                />
+                                            </svg>
+                                            Loading...
+                                        </Fragment>
+                                    ) : (
+                                        "Highlight Rhymes"
+                                    )}
+                                </button>
+                                <button
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        console.log("Garfield");
+                                    }}
+                                    className={clsx(styles.btn, styles.btnBlue)}
+                                    disabled={
+                                        !lyricsInput || isShowRhymePairsLoading
+                                    }
+                                >
+                                    {isShowRhymePairsLoading ? (
+                                        <Fragment>
+                                            <svg
+                                                aria-hidden="true"
+                                                role="status"
+                                                className="inline w-4 h-4 me-3 text-white animate-spin"
+                                                viewBox="0 0 100 101"
+                                                fill="none"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                            >
+                                                <path
+                                                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                                    fill="#E5E7EB"
+                                                />
+                                                <path
+                                                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                                    fill="currentColor"
+                                                />
+                                            </svg>
+                                            Loading...
+                                        </Fragment>
+                                    ) : (
+                                        "Show Rhyme Pairs"
+                                    )}
+                                </button>
+                            </div>
                         </div>
                     </form>
                     <div className={styles.lyricsOutputContainer}>
